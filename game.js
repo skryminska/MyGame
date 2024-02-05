e7a0f4b25f6570114f2a78439bb8755245353694
skryminska <158287265+skryminska@users.noreply.github.com>
2024-02-05 13:00:56 +0200

    функція ground

--- game.js
+++ game.js
@@ -20,6 +20,7 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    //Додаємо зображення
     this.load.image('sky', 'assets/sky.png'); 
     this.load.image('ground', 'assets/platform.png'); 
     this.load.image('star', 'assets/star.png'); 
@@ -31,18 +32,45 @@ function preload ()
 }
 
 var platforms;
+
 function create ()
 {
     this.add.image(400, 300, 'sky'); 
-    platforms = this.physics.add.staticGroup();
 
+    platforms = this.physics.add.staticGroup();
+//Робимо землю статичним тілом 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
-    platforms = this.physics.add.staticGroup();
+// Додаємо персонажа
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
+
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
+
 }
 
 function update ()
