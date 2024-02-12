df94d52b8dc2ae3be78bb97d4700dba5a1b6494c
skryminska <158287265+skryminska@users.noreply.github.com>
2024-02-12 13:53:40 +0200

    added a playier and stars claim

--- game.js
+++ game.js
@@ -16,6 +16,10 @@ var config = {
     }
 };
 
+var player;
+var platforms;
+var cursors;
+
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -70,9 +74,55 @@ this.anims.create({
     frameRate: 10,
     repeat: -1
 });
+        cursors = this.input.keyboard.createCursorKeys()
+
+        stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+        
+        stars.children.iterate(function (child) {
+        
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        
+        });
+
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(stars, platforms);
 
 }
 
 function update ()
 {
+        if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
+
+    function collectStar (player, star)
+    {
+        star.disableBody(true, true);
+    }
+
+
 }
